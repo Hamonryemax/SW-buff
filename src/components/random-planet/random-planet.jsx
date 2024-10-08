dd57fd44d70a8d7d13826f6c9da7130f051b88ad
Maxim Yakushin <hamonryemax@gmail.com>
2024-10-08 17:31:36 +0200

    Add functional for section randomPlanet

--- src/components/random-planet/random-planet.jsx
+++ src/components/random-planet/random-planet.jsx
@@ -1,19 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Spinner from '../spinner/spinner.jsx'
+
+import SwapiService from "../../services/swapi-service.js";
 
 import './random-planet.css';
 
 const RandomPlanet = () => {
 
-    return (
+    const swapiService = new SwapiService();
 
+    const [id, setId] = useState(null);
+    const [name, setName] = useState('Planet');
+    const [population, setPopulation] = useState(null);
+    const [rotationPeriod, setRotationPeriod] = useState(null);
+    const [diameter, setDiameter] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const updatePlanet = ()=> {
+        setLoading(true);
+        const id = Math.floor(Math.random()*20) + 2;
+        swapiService.getPlanet(id)
+            .then((planet) => {
+                setId(id);
+                setName(planet.name);
+                setPopulation(planet.population);
+                setRotationPeriod(planet.rotation_period);
+                setDiameter(planet.diameter);
+                setLoading(false);
+            });
+    };
+
+    useEffect(() => {
+        updatePlanet();
+    }, []);
+
+    if (loading) {
+        return <Spinner />;
+    }
+
+    return (
         <div className="container-randomplanet">
-            <img className="random-planet-img"/>
+            <img className="random-planet-img" alt="Planet"
+                src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}/>
             <div className="container-infoplanet">
                 <ul className="info-planet">
-                    <div className="label-planet">Name Planet</div>
-                    <li className="info-item">population</li>
-                    <li className="info-item">rotation-period</li>
-                    <li className="info-item">diameter</li>
+                    <div className="label-planet">{name}</div>
+                    <li className="info-item">
+                        <span className="term">Population: </span>
+                        <span>{population}</span>
+                    </li>
+                    <li className="info-item">
+                        <span className="term">Rotation Period: </span>
+                        <span>{rotationPeriod}</span>
+                    </li>
+                    <li className="info-item">
+                        <span className="term">Diameter: </span>
+                        <span>{diameter}</span>
+                    </li>
                 </ul>
             </div>
         </div>
---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-eclipse-nq4q5u6dq7r">
+            <div className="ldio-x2uulkbinbj">
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
