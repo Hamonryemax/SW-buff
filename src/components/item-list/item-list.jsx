6a676409a32cc4978705e372b666935a8c3eff1b
Maxim Yakushin <hamonryemax@gmail.com>
2024-10-10 17:08:47 +0200

    Add functional for component item-list

--- src/components/item-list/item-list.jsx
+++ src/components/item-list/item-list.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Spinner from '../spinner/spinner.jsx';
 
 import './item-list.css';
+import SwapiService from "../../services/swapi-service.js";
 
 const ItemList = () => {
-    return (
-        <ul className="list-item">
-            <li className="item">
-                Luke Skywalker
-            </li>
-        </ul>
+    const swapiService = new SwapiService();
 
-    )
+    // Состояния для списка персонажей и загрузки
+    const [peopleList, setPeopleList] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        swapiService
+            .getAllPeople()
+            .then((peopleList) => {
+                setPeopleList(peopleList);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching people:', error);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <Spinner />;
+    }
+
+    return (
+        <div>
+            <ul className="list-item">
+                {peopleList.map((person) => (
+                    <li key={person.id} className="list-group-item list-group-item-action item">
+                        {person.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default ItemList;
